3c85418f1538e91e87e79abd82639be60dadc966
AshwinChandlapur <ashwinyga@rocketmail.com>
2016-03-05 20:35:18 +0530

    Vedic Math Tricks
    Rules Section and multiply numbers done.

---
+++ VedicMath/js.js
@@ -0,0 +1,24 @@
+function start()
+{
+  document.getElementById('rule1').style.visibility='visible';
+  document.getElementById('img1').style.visibility='visible';
+}
+function loadb1()
+{
+  document.getElementById('b1').style.visibility='visible';
+}
+function loadb2()
+{
+  document.getElementById('b2').style.visibility='visible';
+}
+function next()
+{
+  document.getElementById('rule2').style.visibility='visible';
+  document.getElementById('img2').style.visibility='visible';
+
+}
+function next1()
+{
+  document.getElementById('rule3').style.visibility='visible';
+  document.getElementById('img3').style.visibility='visible';
+}
